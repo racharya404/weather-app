924b23fdc474f2edc3c6199437ffaef05c501b19
rojan-ultraship <rojan.acharya@ultraship.ai>
2024-12-09 10:32:53 +0545

    Updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
-const API_KEY = '8d2a110b6ad468ae1a0e459757cf659d';
-const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const API_KEY = 'e57a89a90b6c4693054cfb42d1eb975f';
+//https://api.openweathermap.org/data/2.5/weather?q=Ozark&appid=05af56cf3ac73cc7fe024a774395e9f8
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export const getWeather = async (city, country) => {
-    try {
-        let response = await axios.get(`${API_URL}?q=${city},${country}&appid=${API_KEY}&units=metric`);
-        return response.data;
-    } catch (error) {
-        console.log('Error while calling the api ', error.message);
-        return error.response;
-    }
-}
+  try {
+    let response = await axios.get(`${API_URL}?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    return response.data;
+  } catch (error) {
+    console.log('Error while calling the api ', error.message);
+    return error.response;
+  }
+};
